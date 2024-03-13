- Choose the curve  - Fix errors in velocity computation  - Save some plot for differents omega (0,0.1,1,4)

test.py
@@ -1,4 +1,3 @@
-from typing import Any
 import numpy as np
 from numpy import pi, cos, sin
 from numpy.typing import NDArray
@@ -13,22 +12,25 @@ if test_num == 0:
     DISPLAY_PLOT = True     #: parameter to trigger the plot representation
 
     ## Initialization
-    numpoints = 300                         #: number of points
-    u = np.linspace(-0.2,1.2,numpoints)     #: parametrization
+    numpoints = 600                         #: number of points
+    u = np.linspace(-2.3,-0.4,numpoints)     #: parametrization
     
+    a1 = 1/2
+    w = 2
     # compute the curve
-    x_i = (u+1)**2
-    x_j = sin(u*pi)
-    x_k = x_i * x_j
+    x_i = u**2/a1
+    x_j = sin(w*u)**2
+    x_k = x_i * x_j / 2 
     x = np.array([x_i,x_j,x_k])     #: curve vector
 
 
     # compute the tangent vector to the curve
-    t_i = 2*(u+1)
-    t_j = cos(u*pi)*pi
-    t_k = t_i * x_j + x_i * t_j
+    t_i = 2*u/a1
+    t_j = 2*w*cos(w*u)*sin(w*u)
+    t_k = (t_i*x_j + t_j*x_i) / 2
     # normalize it
     t = np.array([t_i,t_j,t_k])/np.sqrt(t_i**2+t_j**2+t_k**2) 
+    t_i,t_j,t_k = t
 
 
     # set the initial values for the other orthogonal vectors
@@ -44,58 +46,87 @@ if test_num == 0:
     r,s,t = alg.double_reflection(x,t,r,s,numpoints)
 
     # rot
-    ang = 15/180*pi
-    rot_mat = np.array([[cos(ang),-sin(ang),0],
-                        [sin(ang), cos(ang),0],
-                        [0.      , 0.      ,1]])
-    x = np.array([np.dot(rot_mat,x[:,ui]) for ui in range(numpoints)]).T
-    x_i,x_j,x_k = x
+    rot = True
+    if rot:
+        ang = (1+13/180)*pi
+        rot_mat_x = np.array([[1., 0.      , 0.      ],
+                              [0., cos(ang),-sin(ang)],
+                              [0., sin(ang), cos(ang)]])
+        ang = -(15/180)*pi
+        rot_mat_y = np.array([[cos(ang), 0.,sin(ang)],
+                              [0.      , 1., 0.      ],
+                              [-sin(ang), 0., cos(ang)]])
+        ang = (1+0.5/180)*pi
+        rot_mat_z = np.array([[cos(ang),-sin(ang),0],
+                              [sin(ang), cos(ang),0],
+                              [0.      , 0.      ,1]])
+        # rot_mat = np.dot(rot_mat_x,np.dot(rot_mat_z,rot_mat_y))
+        rot_mat = rot_mat_z @ rot_mat_y @ rot_mat_x
+        x = np.array([np.dot(rot_mat,x[:,ui]) for ui in range(numpoints)]).T
+        x_i,x_j,x_k = x
 
-    t = np.array([np.dot(rot_mat,t[:,ui]) for ui in range(numpoints)]).T
-    t /= np.sqrt(np.sum(t**2,axis=0))
-    t_i,t_j,t_k = t
+        t = np.array([np.dot(rot_mat,t[:,ui]) for ui in range(numpoints)]).T
+        t /= np.sqrt(np.sum(t**2,axis=0))
+        t_i,t_j,t_k = t
 
-    r = np.array([np.dot(rot_mat,r[:,ui]) for ui in range(numpoints)]).T
-    r /= np.sqrt(np.sum(r**2,axis=0))
+        r = np.array([np.dot(rot_mat,r[:,ui]) for ui in range(numpoints)]).T
+        r /= np.sqrt(np.sum(r**2,axis=0))
 
-    s = np.array([np.dot(rot_mat,s[:,ui]) for ui in range(numpoints)]).T
-    s /= np.sqrt(np.sum(s**2,axis=0))
+        s = np.array([np.dot(rot_mat,s[:,ui]) for ui in range(numpoints)]).T
+        s /= np.sqrt(np.sum(s**2,axis=0))
+
+    # compute the matrix to change frame reference
+    mat = [np.array([r[:,ui],s[:,ui],t[:,ui]]).T for ui in range(numpoints) ]
 
     ## Computation of the streamlines
     R = 0.2     #: cylinder radius
+    omega = 4
     
     def compute_stream(R: float = R,th0: float = 0) -> NDArray:
         """Function to compute a streamline along the curve
         """
+        ui = np.linspace(0,2,numpoints)
         # compute the streamline in the frame of the curve
-        stream = np.array([R*cos(u*pi + th0*pi),R*sin(u*pi + th0*pi),np.zeros(numpoints)])
-        # compute the matrix to change frame reference
-        mat = [np.array([r[:,ui],s[:,ui],t[:,ui]]).T for ui in range(numpoints) ]
+        stream = np.array([R*cos(omega*ui*pi + th0*pi),R*sin(omega*ui*pi + th0*pi),np.zeros(numpoints)])
         # compute the cartesian coordinates of the streamline
         return np.array([ np.dot(mat[ui],stream[:,ui]) for ui in range(numpoints)]).T + x
     
     th_max = 1.98
-    num = 40
+    num = 50
     streamlines = np.array([compute_stream(R=R,th0=th0) for th0 in np.linspace(0,th_max,num)])
 
     ## Compute the velocity field
     # uniform field case
     V = 3
-    v = np.array([V*t]*num)
+
+    def compute_velocity(R: float = R,th0: float = 0) -> NDArray:
+        """Function to compute a streamline along the curve
+        """
+        ui = np.linspace(0,2,numpoints)
+        vT = np.sqrt(V**2 - (omega*R*pi)**2)
+        # compute the streamline in the frame of the curve
+        velox = np.array([-omega*R*pi*sin(omega*ui*pi + th0*pi),omega*R*pi*cos(omega*ui*pi + th0*pi),[vT]*numpoints])
+        # compute the cartesian coordinates of the streamline
+        return np.array([ np.dot(mat[ui],velox[:,ui]) for ui in range(numpoints)]).T 
+
+    v = np.array([compute_velocity(R=R,th0=th0) for th0 in np.linspace(0,th_max,num)])
 
     ## Set maps
 
     i = (1.75,3.00)
-    j = (1.20,1.90)
-    k = (1.80,2.80)
+    j = (0.80,1.45)
+    k = (7.10,8.00)
 
     cond_str_i = (streamlines[:,0] >= i[0]) & (streamlines[:,0] <= i[1])
     cond_str_j = (streamlines[:,1] >= j[0]) & (streamlines[:,1] <= j[1])
     cond_str_k = (streamlines[:,2] >= k[0]) & (streamlines[:,2] <= k[1])
 
-    indx_i = np.where(cond_str_j & cond_str_k)
-    indx_j = np.where(cond_str_k & cond_str_i)
-    indx_k = np.where(cond_str_i & cond_str_j)
+    # indx_i = np.where(cond_str_j & cond_str_k)
+    # indx_j = np.where(cond_str_k & cond_str_i)
+    # indx_k = np.where(cond_str_i & cond_str_j)
+    indx_i = None
+    indx_j = None
+    indx_k = None
      
 
     ## Plotting    
@@ -104,21 +135,23 @@ if test_num == 0:
 
         if PLOT_PARAM == 0:
             
-            plots = '2'
+            plots = '0'
             if plots == 'all' or '0' in plots:
                 import mayavi.mlab as mlab
-
-                for stream in streamlines:
-                    mlab.plot3d(*stream,color=(0,0,1),tube_radius=0.01)
+                # pl1 = mlab.plot3d(*x,color=(0,1,0),name='cylinder axis',tube_radius=R)
+                pl2 = mlab.plot3d(*x,color=(0,1,0),name='cylinder axis',tube_radius=None)
+                for (stream,c) in zip(streamlines,np.linspace(0,1,num)):
+                    mlab.plot3d(*stream,color=(c,0,1),tube_radius=0.01)
                 
                 mlab.show()
 
             vmin, vmax = v.min(), v.max()
-            binning = 50 # np.linspace(vmin,vmax,50)
+            binning = 40 # np.linspace(vmin,vmax,50)
+            print(vmin,vmax)
             if plots == 'all' or '1' in plots:
                 fig, ax = plt.subplots(2,3)
 
-                fig.suptitle(f'{num} streamlines with {numpoints} points: projections $\ell = i, j, k$')
+                fig.suptitle(f'{num} streamlines with {numpoints} points\n$\ell = i, j, k$, bins num: {binning}, $\\omega =$ {omega}, '+'uniform velocity: $|\\vec{V}| =$ '+f'{V}')
 
                 axi = ax[:,0]
                 axj = ax[:,1]
@@ -127,29 +160,29 @@ if test_num == 0:
                 axi[0].set_title('Plane $j$-$k$')
                 for ui in range(numpoints):
                     axi[0].scatter(streamlines[:,1,ui],streamlines[:,2,ui],c=v[:,0,ui],vmin=vmin,vmax=vmax,cmap='RdBu')
-                axi[0].plot([j[0],j[0],j[1],j[1],j[0]],[k[0],k[1],k[1],k[0],k[0]],color='green')
+                # axi[0].plot([j[0],j[0],j[1],j[1],j[0]],[k[0],k[1],k[1],k[0],k[0]],color='green')
                 axi[0].set_xlabel('$j$')
                 axi[0].set_ylabel('$k$')
-                axi[1].hist(v[:,0,indx_i].flatten(),bins=binning)
+                axi[1].hist(v[:,0,:].flatten(),bins=binning)
                 axi[1].set_xlabel('$v_i$')
                 axi[1].set_ylabel('$counts$')
             
                 axj[0].set_title('Plane $k$-$i$')
                 for ui in range(numpoints):
                     axj[0].scatter(streamlines[:,2,ui],streamlines[:,0,ui],c=v[:,1,ui],vmin=vmin,vmax=vmax,cmap='RdBu')
-                axj[0].plot([k[0],k[0],k[1],k[1],k[0]],[i[0],i[1],i[1],i[0],i[0]],color='green')
+                # axj[0].plot([k[0],k[0],k[1],k[1],k[0]],[i[0],i[1],i[1],i[0],i[0]],color='green')
                 axj[0].set_xlabel('$k$')
                 axj[0].set_ylabel('$i$')
-                axj[1].hist(v[:,1,indx_j].flatten(),bins=binning)
+                axj[1].hist(v[:,1,:].flatten(),bins=binning)
                 axj[1].set_xlabel('$v_j$')
                 
                 axk[0].set_title('Plane $i$-$j$')
                 for ui in range(numpoints):
                     plot_k = axk[0].scatter(streamlines[:,0,ui],streamlines[:,1,ui],c=v[:,2,ui],vmin=vmin,vmax=vmax,cmap='RdBu')
-                axk[0].plot([i[0],i[0],i[1],i[1],i[0]],[j[0],j[1],j[1],j[0],j[0]],color='green')
+                # axk[0].plot([i[0],i[0],i[1],i[1],i[0]],[j[0],j[1],j[1],j[0],j[0]],color='green')
                 axk[0].set_xlabel('$i$')
                 axk[0].set_ylabel('$j$')
-                axk[1].hist(v[:,2,indx_k].flatten(),bins=binning)
+                axk[1].hist(v[:,2,:].flatten(),bins=binning)
                 axk[1].set_xlabel('$v_k$')
 
                 cbaxes = fig.add_axes([0.92, 0.1, 0.02, 0.8])  
@@ -159,7 +192,7 @@ if test_num == 0:
 
             if plots == 'all' or '2' in plots:
                 fig = plt.figure()
-                fig.suptitle('Plane $j$-$k$')
+                fig.suptitle(f'Plane $j$-$k$ - $\\omega =$ {omega}')
                 ax0 = fig.add_subplot(1,2,1)
                 for ui in range(numpoints):
                     pp = ax0.scatter(streamlines[:,1,ui],streamlines[:,2,ui],c=v[:,0,ui],vmin=vmin,vmax=vmax,cmap='RdBu')
@@ -167,9 +200,9 @@ if test_num == 0:
                 ax0.set_ylabel('$k$')
                 
                 # map1 = ((0.35,0.75),(0.9,1.55))
-                map1 = ((1.25,1.60),(1.7,2.1))
-                map2 = (j,k)
-                map3 = ((0.63,0.93),(-2.2,-1.6))
+                map1 = ((-0.15,0.17),(0.7,1.4))
+                map2 = ((0.6,1.),(-0.6,-0.2))
+                map3 = ((-0.2,0.05),(-0.90,-0.37))
                 
                 ax0.plot([map1[0][0],map1[0][0],map1[0][1],map1[0][1],map1[0][0]],[map1[1][0],map1[1][1],map1[1][1],map1[1][0],map1[1][0]], color='green',label='map1')
                 ax0.plot([map2[0][0],map2[0][0],map2[0][1],map2[0][1],map2[0][0]],[map2[1][0],map2[1][1],map2[1][1],map2[1][0],map2[1][0]], color='violet',label='map2')