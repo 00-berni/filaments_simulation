dbe0de6750b8eae1a32e4ed1b817df6cfcd503fb
00-berni <bernardo.vettori@outlook.it>
2024-03-14 23:57:27 +0100

    - Change package name

--- test.py
+++ test.py
@@ -2,7 +2,7 @@ import numpy as np
 from numpy import pi, cos, sin
 from numpy.typing import NDArray
 import matplotlib.pyplot as plt
-import filpy.alglin as alg
+import filpy.geobj as geobj
 
 
 test_num = 0
@@ -12,7 +12,7 @@ if test_num == 0:
     DISPLAY_PLOT = True     #: parameter to trigger the plot representation
 
     ## Initialization
-    numpoints = 600                         #: number of points
+    numpoints = 500                          #: number of points
     u = np.linspace(-2.3,-0.4,numpoints)     #: parametrization
     
     a1 = 1/2
@@ -43,7 +43,7 @@ if test_num == 0:
     s /= np.sqrt(np.sum(s**2,axis=0))
 
     # compute the frame along the curve via the double reflection method
-    r,s,t = alg.double_reflection(x,t,r,s,numpoints)
+    r,s,t = geobj.double_reflection(x,t,r,s)
 
     # rot
     rot = True
@@ -62,6 +62,7 @@ if test_num == 0:
                               [0.      , 0.      ,1]])
         # rot_mat = np.dot(rot_mat_x,np.dot(rot_mat_z,rot_mat_y))
         rot_mat = rot_mat_z @ rot_mat_y @ rot_mat_x
+        
         x = np.array([np.dot(rot_mat,x[:,ui]) for ui in range(numpoints)]).T
         x_i,x_j,x_k = x
 
@@ -80,7 +81,7 @@ if test_num == 0:
 
     ## Computation of the streamlines
     R = 0.2     #: cylinder radius
-    omega = 4
+    omega = 0
     
     def compute_stream(R: float = R,th0: float = 0) -> NDArray:
         """Function to compute a streamline along the curve
@@ -92,8 +93,9 @@ if test_num == 0:
         return np.array([ np.dot(mat[ui],stream[:,ui]) for ui in range(numpoints)]).T + x
     
     th_max = 1.98
-    num = 50
-    streamlines = np.array([compute_stream(R=R,th0=th0) for th0 in np.linspace(0,th_max,num)])
+    num = 10
+    rad = 1
+    streamlines = np.array([compute_stream(R=Ri,th0=th0) for th0 in np.linspace(0,th_max,num) for Ri in np.linspace(0.05,R,rad)])
 
     ## Compute the velocity field
     # uniform field case
@@ -109,7 +111,7 @@ if test_num == 0:
         # compute the cartesian coordinates of the streamline
         return np.array([ np.dot(mat[ui],velox[:,ui]) for ui in range(numpoints)]).T 
 
-    v = np.array([compute_velocity(R=R,th0=th0) for th0 in np.linspace(0,th_max,num)])
+    v = np.array([compute_velocity(R=Ri,th0=th0) for th0 in np.linspace(0,th_max,num) for Ri in np.linspace(0.05,R,rad)])
 
     ## Set maps
 
@@ -135,18 +137,18 @@ if test_num == 0:
 
         if PLOT_PARAM == 0:
             
-            plots = '0'
+            plots = '2'
             if plots == 'all' or '0' in plots:
                 import mayavi.mlab as mlab
                 # pl1 = mlab.plot3d(*x,color=(0,1,0),name='cylinder axis',tube_radius=R)
                 pl2 = mlab.plot3d(*x,color=(0,1,0),name='cylinder axis',tube_radius=None)
-                for (stream,c) in zip(streamlines,np.linspace(0,1,num)):
+                for (stream,c) in zip(streamlines,np.linspace(0,1,num*rad)):
                     mlab.plot3d(*stream,color=(c,0,1),tube_radius=0.01)
                 
                 mlab.show()
 
             vmin, vmax = v.min(), v.max()
-            binning = 40 # np.linspace(vmin,vmax,50)
+            binning = 40
             print(vmin,vmax)
             if plots == 'all' or '1' in plots:
                 fig, ax = plt.subplots(2,3)
@@ -200,7 +202,7 @@ if test_num == 0:
                 ax0.set_ylabel('$k$')
                 
                 # map1 = ((0.35,0.75),(0.9,1.55))
-                map1 = ((-0.15,0.17),(0.7,1.4))
+                map1 = ((-0.25,0.2),(0.9,1.6))
                 map2 = ((0.6,1.),(-0.6,-0.2))
                 map3 = ((-0.2,0.05),(-0.90,-0.37))
                 
